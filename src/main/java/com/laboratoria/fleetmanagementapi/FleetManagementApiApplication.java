ec2f09bb8e4eb628e24e042921ae5b4d8adebe21
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-11-08 14:14:48 -0300

    inciando o projeto

---
+++ src/main/java/com/laboratoria/fleetmanagementapi/FleetManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.laboratoria.fleetmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laboratoria/fleetmanagementapi/FleetManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratoria.fleetmanagementapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

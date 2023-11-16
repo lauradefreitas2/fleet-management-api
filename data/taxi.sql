61957c3be8f030c47eb708d58286aca9c50ea355
lauradefreitas2 <lauradovalegameplays@gmail.com>
2023-11-16 16:41:03 -0300

    criação do DB

---
+++ data/taxi.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS taxis (
+   id INT PRIMARY KEY,
+   placa VARCHAR (10) NOT NULL
+);
+
+INSERT INTO taxis (id, placa)
+VALUES
+    (7249, 'CNCJ-2997'),
+    (10133, 'PAOF-6727');
+
+
+INSERT INTO taxis (id, placa)
+VALUES
+    (2210, 'FGMG-3071'),
+    (1065, 'GHDN-9291'),
+    (7956, 'CCKF-1601'),
+    (252, 'LAHG-7611'),
+    (9557, 'BOIG-0354'),
+    (4485, 'LLEL-9271')
+    (6418, 'GHGH-1458');
